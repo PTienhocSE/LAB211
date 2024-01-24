9229af029bbce488888eaa59aee10fe309c6b4c2
Tran Phuc Tien <phuctienrt@gmail.com>
2024-01-24 09:12:13 +0700

    fix Text Main

--- TextManagement/src/controller/Programming.java
+++ TextManagement/src/controller/Programming.java
@@ -17,27 +17,22 @@ public class Programming {
     }
 
     public void run() {
-        // Read file
         System.out.print("Enter input file name: ");
         String inputFilename = scanner.nextLine();
         Content content = library.readFile(inputFilename);
 
-        // Check if the content is empty
         if (content.isEmpty()) {
             System.out.println("The input file is empty.");
         } else {
-            // Display original content
+
             System.out.println("Original content:");
             System.out.print(content.getText());
 
-            // Format content
             contentController.formatContent(content);
 
-            // Display formatted content
             System.out.println("\nFormatted content:");
             System.out.println(content.getText());
 
-            // Write formatted content to another file
             System.out.print("Enter output file name: ");
             String outputFilename = scanner.nextLine();
             library.writeFile(outputFilename, content.getText(), false);
